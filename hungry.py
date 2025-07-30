bacde2f9cbd56261b7db22417da18595be74e5d8
Dell Moore <delmer0818@yahoo.com>
2025-07-30 09:53:57 -0400

    water

--- hungry.py
+++ hungry.py
@@ -2,4 +2,6 @@ hungry=input("are you hungry?")
 if hungry=="yes":
 	print("eat something")
 else:
-	print("do your homework")
+	thirsty=input("are you thirsty?")
+	if thirsty=="yes":
+		print("drink something")
